641f88964513ac88ab9e09ac3d0951500f8a4c18
Ronnit3012 <ronnit.mirgh@gmail.com>
2023-01-23 16:45:09 +0530

    folder structure and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+import HomePage from './scenes/homePage/index';
+import LoginPage from './scenes/loginPage/index';
+import ProfilePage from './scenes/profilePage/index';
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />}></Route>
+          <Route path="/home" element={<HomePage />}></Route>
+          <Route path="/profile/:userId" element={<ProfilePage />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
