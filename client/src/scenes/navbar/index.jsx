ded6948165cc0a61458095909dd55676ce8a623c
Ronnit3012 <ronnit.mirgh@gmail.com>
2023-01-27 04:03:52 +0530

    register, login pages, and form

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -39,11 +39,8 @@ const Navbar = () => {
   const primaryLight = theme.palette.primary.light;
   const alt = theme.palette.background.alt;
 
-  const fullName = 'Ronnit Mirgh';
-  if(user) {
-    const fullName = `${user.firstName} ${user.lastName}`;
-    console.log(fullName);
-  }
+  const fullName = `${user.firstName} ${user.lastName}`;
+  console.log(fullName);
 
   return (
     <FlexBetween padding="1rem 6%" backgroundColor={alt}>
